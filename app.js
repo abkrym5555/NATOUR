b482b7b68188eade802f59f738c254d36935c32d
abkrym5555 <momo542004@gmail.com>
2026-06-28 20:58:51 +0300

    feat:access static files

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const usersRouter = require('./routes/userRoutes');
 // middleware to make req body readable
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(express.static(`${__dirname}/public`));
 
 // ROUTES
 app.use('/api/v1/tours', toursRouter);
