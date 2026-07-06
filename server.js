6756c1599e471db2e1adf8fb74c2ddab616b29fe
abkrym5555 <momo542004@gmail.com>
2026-07-06 16:28:01 +0300

    feat:connect to cloud DB

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<db_password>',
+  process.env.DATABASE_PASSWORD,
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successfuly'));
 
 const port = process.env.PORT || 3000;
 // START SERVER
