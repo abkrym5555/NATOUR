a2b9e06ca00298bf577e27eeb9d18768b93bac40
abkrym5555 <momo542004@gmail.com>
2026-07-08 17:50:05 +0300

    feat:make funs work with mongooso cloud

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -1,46 +1,90 @@
 const Tour = require('../models/tourModel');
 
-const getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    // result: tours.length,
-    data: {
-      // tours,
-    },
-  });
+const getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'success',
+      result: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
 
-const getTourById = (req, res) => {
-  // const tourId = +req.params.id;
-  // const targetTour = tours.find((tur) => tur.id === tourId);
-  res.status(200).json({
-    status: 'success',
-    data: {
-      // tour: targetTour,
-    },
-  });
+const getTourById = async (req, res) => {
+  try {
+    const targetTour = await Tour.findById(req.params.id);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour: targetTour,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
 
-const creatNewTour = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-    data: {
-      // tour: newTour,
-    },
-  });
+const creatNewTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
 
-const editTour = (req, res) => {
-  return res.status(200).json({
-    status: 'success',
-    data: {
-      // tour: upTour,
-    },
-  });
+const editTour = async (req, res) => {
+  try {
+    const upTour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    return res.status(200).json({
+      status: 'success',
+      data: {
+        tour: upTour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
 
-const deleteTour = (req, res) => {
-  res.status(404).end('can not delete in the file');
+const deleteTour = async (req, res) => {
+  try {
+    const delTour = await Tour.findOneAndDelete({ _id: req.params.id });
+    res.status(200).json({
+      status: 'success',
+      deletedTour: delTour,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: error,
+    });
+  }
 };
 
 module.exports = {
