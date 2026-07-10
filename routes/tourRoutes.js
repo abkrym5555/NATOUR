db9a4c390427597fb34a39e0d4ac0e1da3286930
abkrym5555 <momo542004@gmail.com>
2026-07-11 01:26:39 +0300

    feat:add the aggregation

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,10 +6,14 @@ const {
   getAllTours,
   getTourById,
   aliasTopTours,
+  getTourStats,
+  getMonthlyPlan,
 } = require('../controllers/tourControllers');
 
 const route = express.Router();
 
+route.route('/tours-stats').get(getTourStats);
+route.route('/monthly-plan/:year').get(getMonthlyPlan);
 route.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 route.route('/').get(getAllTours).post(creatNewTour);
 route.route('/:id').get(getTourById).patch(editTour).delete(deleteTour);
