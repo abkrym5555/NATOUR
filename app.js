3444ac38592d606f90a3896b7074641bcdb73fd7
abkrym5555 <momo542004@gmail.com>
2026-07-16 18:00:15 +0300

    handel unexpect route

--- app.js
+++ app.js
@@ -13,4 +13,10 @@ app.use(express.static(`${__dirname}/public`));
 app.use('/api/v1/tours', toursRouter);
 app.use('/api/v1/users', usersRouter);
 
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'failed',
+    message: `The ${req.originalUrl} not found `,
+  });
+});
 module.exports = app;
