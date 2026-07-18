992bed9f74e521dce90c9941fb6611f9f4cb855b
abkrym5555 <momo542004@gmail.com>
2026-07-18 19:04:28 +0300

    fix:use async await

--- models/userModel.js
+++ models/userModel.js
@@ -35,10 +35,10 @@ const userSchema = new mongoose.Schema({
 });
 
 // doc middleware to encrypt the pass
-userSchema.pre('save', function (next) {
+userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
 
-  this.password = bcrypt.hash(this.password, 12);
+  this.password = await bcrypt.hash(this.password, 12);
   //to remove this from doc
   this.passwordConfirm = undefined;
   next();
