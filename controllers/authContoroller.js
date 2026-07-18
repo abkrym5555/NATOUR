1ced1b89acd4a335448ea4fde59da0eda12ad48d
abkrym5555 <momo542004@gmail.com>
2026-07-18 19:20:57 +0300

    fix:signup fun

--- controllers/authContoroller.js
+++ controllers/authContoroller.js
@@ -2,7 +2,12 @@ const User = require('../models/userModel');
 const catchAsyncError = require('../utils/catchAsyncError');
 
 const signUp = catchAsyncError(async (req, res, next) => {
-  const newUser = User.create(req.body);
+  const newUser = User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
 
   res.status(201).json({
     status: 'success',
