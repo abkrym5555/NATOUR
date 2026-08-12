ecc2dde5040eca873f7f720439c36fe60df31a6a
abkrym5555 <momo542004@gmail.com>
2026-08-12 14:46:06 +0300

    feat: implement static method to calculate and update average rating for tours based on reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const Tour = require('./tourModel');
 const reviewSchema = new mongoose.Schema(
   {
     review: {
@@ -41,6 +41,33 @@ reviewSchema.pre(/^find/, function (next) {
   next();
 });
 
+// static model
+reviewSchema.statics.calcAverageRating(async function (tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: {
+        tour: tourId,
+      },
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+
+  await Tour.findByIdAndUpdate(tourId, {
+    ratingsAverage: stats[0].avgRating,
+    ratingsQuantity: stats[0].nRating,
+  });
+});
+
+//call static model
+reviewSchema.post('save', function () {
+  this.constructor.calcAverageRating(this.tour);
+});
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
